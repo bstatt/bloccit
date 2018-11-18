f58f392a8bb94a92f616d67f600ed15d5f77dbc5
Britany Statt <bstatt2@gmail.com>
2018-11-17 21:13:32 -0700

    topic part 1 associations complete

---
+++ spec/integration/topics_spec.js
@@ -0,0 +1,20 @@
+const request = require("request");
+const server = require("../../src/server");
+const base = "http://localhost:3000/topics/";
+// const sequelize = require("../../src/db/models/index").sequelize;
+// const Topic = require("../../src/db/models").Topic;
+
+
+describe("routes : topics", () => {
+
+  describe("GET /topics", () => {
+
+    it("should return a status code 200", (done) => {
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        done();
+      });
+    });
+
+  });
+});
