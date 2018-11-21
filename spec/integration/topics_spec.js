c67f345f0b3fd66910f37d04f4c2a5e362533b91
Britany Statt <bstatt2@gmail.com>
2018-11-20 20:52:36 -0700

    topic resource part 1 checkpoint complete

--- spec/integration/topics_spec.js
+++ spec/integration/topics_spec.js
@@ -7,34 +7,35 @@ const Topic = require("../../src/db/models").Topic;
 
 describe("routes : topics", () => {
 
-  // beforeEach((done) => {
-  //   this.topic;
-  //   sequelize.sync({force: true}).then((res) => {
-  //
-  //    Topic.create({
-  //      title: "JS Frameworks",
-  //      description: "There is a lot of them"
-  //    })
-  //     .then((topic) => {
-  //       this.topic = topic;
-  //       done();
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //       done();
-  //     });
-  //   });
-  // });
+  beforeEach((done) => {
+    this.topic;
+    sequelize.sync({force: true}).then((res) => {
+
+     Topic.create({
+       title: "JS Frameworks",
+       description: "There is a lot of them"
+     })
+      .then((topic) => {
+        this.topic = topic;
+        done();
+      })
+      .catch((err) => {
+        console.log(err);
+        done();
+      });
+    });
+  });
 
   describe("GET /topics", () => {
 
-    it("should return a status code 200", (done) => {
-      console.log("Starting request...");
+    it("should return a status code 200 and all topics", (done) => {
       request.get(base, (err, res, body) => {
-        console.log("Request finished");
         expect(res.statusCode).toBe(200);
+        expect(err).toBeNull();
+        expect(body).toContain("Topics");
+        expect(body).toContain("JS Frameworks");
         done();
       });
-  });
+    });
    });
   });
