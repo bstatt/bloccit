7c934f4f31a81a85db58bd7c407b48a03b7aaed7
Britany Statt <bstatt2@gmail.com>
2018-11-27 19:39:27 -0700

    topic part 2 checkpoint complete

---
+++ src/db/seeds/20181128023253-topics.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const faker = require("faker");
+
+let topics = [];
+
+for(let i = 1 ; i <= 15 ; i++){
+  topics.push({
+    title: faker.hacker.noun(),
+    description: faker.hacker.phrase(),
+    createdAt: new Date(),
+    updatedAt: new Date()
+  });
+}
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert("Topics", topics, {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("Topics", null, {});
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};
